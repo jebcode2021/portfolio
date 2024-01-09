f8d49fb32ef0e910952f24384224282542fba575
jebcode2021 <jebcode@outlook.com.br>
2024-01-09 12:55:02 -0300

    first commit

---
+++ src/Teste.tsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+type BotaoProps = {
+  principal: boolean
+  fontSize?: string
+}
+
+const Botao = styled.button<BotaoProps>`
+  background-color: ${(props) => (props.principal ? 'red' : 'blue')};
+  font-size: ${(props) => props.fontSize || '12px'};
+`
+
+const BotaoPerigo = styled(Botao)`
+  background-color: red;
+  color: #fff;
+
+  span {
+    text-decoration: line-through;
+  }
+
+  &:hover {
+    background-color: #fff;
+    color: red;
+  }
+`
+
+function Teste() {
+  return (
+    <>
+      <Botao principal>Enviar</Botao>
+      <Botao fontSize="14px" principal={false}>
+        Cancelar
+      </Botao>
+      <BotaoPerigo as="a" principal>
+        <span>Excluir</span>
+      </BotaoPerigo>
+    </>
+  )
+}
+
+export default Teste
