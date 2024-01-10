c937f005667f3d69503863bda0fab97e9a352a77
jebcode2021 <jebcode@outlook.com.br>
2024-01-10 00:58:26 -0300

    Desafio github

--- src/components/Titulo/styles.ts
+++ src/components/Titulo/styles.ts
@@ -6,4 +6,16 @@ export const Titulo = styled.h3<Props>`
   font-size: ${(props) => (props.fontSize ? props.fontSize + 'px' : '14px')};
   font-weight: bold;
   margin-bottom: 16px;
+  text-align: center;
+  text-transform: uppercase;
+
+  &::after {
+    content: '';
+    display: block;
+    width: 20px;
+    height: 4px;
+    background-color: ${(props) => props.theme.corPrincipal};
+    margin: 0 auto;
+    margin-top: 8px;
+  }
 `
